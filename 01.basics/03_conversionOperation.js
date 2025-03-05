f9b13358b655244c36baa80e3f586fabf0760c81
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-03-05 18:50:13 +0530

    completed till lecture no.6

---
+++ 01.basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "kashyap";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log((valueInNumber));
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanInLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanInLoggedIn);
+
+// 1 => true; 0 => false
+// """ => false
+// "Kashyap" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
