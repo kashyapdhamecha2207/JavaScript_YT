6554b97c41469a6fdc51fcfdaed76c36cbe23fbc
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-03-05 19:08:24 +0530

    completed till lecture no.7

--- 01.basics/03_conversionOperation.js
+++ 01.basics/03_conversionOperation.js
@@ -24,5 +24,35 @@ let booleanInLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// Operations
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = "Kashyap";
+
+let str3 = str1 + str2;
+//  console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+//  console.log( (3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
