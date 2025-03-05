b09f86f6c6c25957644ad05e671d3ff854aace1c
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-03-05 19:22:53 +0530

    completed till lecture no.8

---
+++ 01.basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// null >= 0 => true 
+// bcz comparison operators(> , <) converts null to number and treat it as 0
+
+
+// === (strictly checks)
+console.log("2" == 2);  // true
+console.log("2" === 2); // false
+
+// this all will show "False"
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
