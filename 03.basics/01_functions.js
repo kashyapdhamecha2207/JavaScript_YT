aac090365be470afa9322c51c1dc266668829874
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-03-09 05:45:24 +0530

    completed till lecture no.20

--- 03.basics/01_functions.js
+++ 03.basics/01_functions.js
@@ -1,4 +1,4 @@
-function sayMyName(){
+function sayMyName() {
     console.log("K");
     console.log("A");
     console.log("S");
@@ -30,13 +30,44 @@ function sayMyName(){
 
 // console.log("Result: ", result);
 
-function loginUserMessage(username){
-    if(!username){
+function loginUserMessage(username) {
+    if (!username) {
         console.log("Please provide a username")
         return
     }
-    return `${username} just logged in` 
+    return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("Kashyap"))
-console.log(loginUserMessage())       // will print undefined just logged in
+// console.log(loginUserMessage())       // will print undefined just logged in
+
+
+// function calculateCartePrice(val1, val2, ...num1) {
+//     return num1     // [ 500, 2000 ]
+// }
+
+// console.log(calculateCartePrice(100, 200, 500, 2000));
+
+const user = {
+    username: "Kashyap",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username in ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+} 
+
+console.log(returnSecondValue(myNewArray));
