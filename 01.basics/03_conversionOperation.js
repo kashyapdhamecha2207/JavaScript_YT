7f2e126f9f90d5ba6b444488558bbcd9a59a1af7
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-03-18 11:00:14 +0530

    completed till lecture no.30

--- 01.basics/03_conversionOperation.js
+++ 01.basics/03_conversionOperation.js
@@ -22,10 +22,9 @@ let booleanInLoggedIn = Boolean(isLoggedIn);
 // "Kashyap" => true
 
 let someNumber = 33
-
 let StringNumber = String(someNumber);
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
 
 // Operations
 
@@ -34,7 +33,7 @@ let negValue = -value;
 // console.log(negValue);
 
 let str1 = "Hello";
-let str2 = "Kashyap";
+let str2 = " Kashyap";
 
 let str3 = str1 + str2;
 //  console.log(str3);
@@ -45,8 +44,8 @@ let str3 = str1 + str2;
 //  console.log(1 + 2 + "2");
 //  console.log( (3+4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(-true);
+// console.log(-"");
 
 let num1,num2,num3;
 
@@ -54,5 +53,5 @@ num1 = num2 = num3 = 2+2
 
 let gameCounter=100
 gameCounter++
-console.log(gameCounter);
+// console.log(gameCounter);
 
--- 01.basics/04_comparison.js
+++ 01.basics/04_comparison.js
@@ -5,12 +5,12 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);      // true
+console.log("02" > 1);     // true
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);
+console.log(null > 0);     // false
+console.log(null == 0);    // false
+console.log(null >= 0);    // true
 
 // null >= 0 => true 
 // bcz comparison operators(> , <) converts null to number and treat it as 0
@@ -21,6 +21,6 @@ console.log("2" == 2);  // true
 console.log("2" === 2); // false
 
 // this all will show "False"
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);  // false 
+console.log(undefined > 0);   // false
+console.log(undefined < 0);   // false
